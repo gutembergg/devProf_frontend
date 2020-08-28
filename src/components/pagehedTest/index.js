035f6a9eda26b05a67035f303e80c50aea0ef0b4
Gutembergue Mascarenhas <gmascarenhas3001@gmail.com>
2020-08-28 23:19:27 +0200

    test

--- src/components/pagehedTest/index.js
+++ src/components/pagehedTest/index.js
@@ -1,16 +1,13 @@
 import React from 'react'
 import { FiArrowLeft } from 'react-icons/fi'
 
-import Select from '../Select'
-import Input from '../Input'
-
 import './styles.css'
 
-const Pagehedetest = ({ title, description }) => {
+const Pagehedetest = ({ title, description, children }) => {
   return (
     <>
       <div className="page-header">
-        <div className="row d-flex justify-content-between">
+        <div className="row d-flex justify-content-between test">
           <FiArrowLeft
             style={{ marginLeft: '2rem', color: '#f0f0f7', cursor: 'pointer' }}
             size="4rem"
@@ -20,50 +17,11 @@ const Pagehedetest = ({ title, description }) => {
             DevProf
           </div>
         </div>
-        <div className="container">
-          <div className="row d-flex align-self-center title-text">{title}</div>
+        <div className="wrap-text">
+          <div className="d-flex align-self-center col-sm-6 col-md-6 title-text">{title}</div>
         </div>
       </div>
-
-      <div className="row header-form d-flex justify-content-center">
-        <div className="col-sm-12 col-md-2">
-          <Select
-            classeWeekDay="select_block"
-            name="languages"
-            label="Langages"
-            options={[
-              { value: 'PHP', label: 'PHP' },
-              { value: 'Java', label: 'Java' },
-              { value: 'JavaScript', label: 'JavaScript' },
-              { value: 'Python', label: 'Python' },
-              { value: 'CMS', label: 'CMS' },
-              { value: 'HTML', label: 'HTML' }
-            ]}
-          />
-        </div>
-        <div className="col-sm-12 col-md-2">
-          <Select
-            classeWeekDay="select_block"
-            name="week_day"
-            label="Jour"
-            options={[
-              { value: '0', label: 'Dimanche' },
-              { value: '1', label: 'Lundi' },
-              { value: '2', label: 'Mardi' },
-              { value: '3', label: 'Mercredi' },
-              { value: '4', label: 'Jeudi' },
-              { value: '5', label: 'Vendredi' },
-              { value: '6', label: 'Samedi' }
-            ]}
-          />
-        </div>
-        <div className="col-sm-12 col-md-2">
-          <Input type="time" name="time" label="Horaire" />
-        </div>
-        <div className="col-sm-12 col-md-2">
-          <button className="btn btn-success">Search</button>
-        </div>
-      </div>
+      {children}
     </>
   )
 }
--- src/pages/homeTest/index.js
+++ src/pages/homeTest/index.js
@@ -1,11 +1,53 @@
 import React from 'react'
 import Pagehedetest from '../../components/pagehedTest'
 
+import Select from '../../components/Select'
+import Input from '../../components/Input'
+
 const HomeTest = () => {
   return (
     <div>
-      <Pagehedetest title="Voici les Devs disponibles." />
-      <div></div>
+      <Pagehedetest title="Voici les Devs disponibles.">
+        <div className="row header-form d-flex justify-content-center">
+          <div className="col-sm-12 col-md-2">
+            <Select
+              classeWeekDay="select_block"
+              name="languages"
+              label="Langages"
+              options={[
+                { value: 'PHP', label: 'PHP' },
+                { value: 'Java', label: 'Java' },
+                { value: 'JavaScript', label: 'JavaScript' },
+                { value: 'Python', label: 'Python' },
+                { value: 'CMS', label: 'CMS' },
+                { value: 'HTML', label: 'HTML' }
+              ]}
+            />
+          </div>
+          <div className="col-sm-12 col-md-2">
+            <Select
+              classeWeekDay="select_block"
+              name="week_day"
+              label="Jour"
+              options={[
+                { value: '0', label: 'Dimanche' },
+                { value: '1', label: 'Lundi' },
+                { value: '2', label: 'Mardi' },
+                { value: '3', label: 'Mercredi' },
+                { value: '4', label: 'Jeudi' },
+                { value: '5', label: 'Vendredi' },
+                { value: '6', label: 'Samedi' }
+              ]}
+            />
+          </div>
+          <div className="col-sm-12 col-md-2">
+            <Input type="time" name="time" label="Horaire" />
+          </div>
+          <div className="col-sm-12 col-md-2">
+            <button className="btn btn-success">Search</button>
+          </div>
+        </div>
+      </Pagehedetest>
     </div>
   )
 }
